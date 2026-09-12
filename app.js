9b9caf936ffb29ab72ac0eff57f34b47f01b6641
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-12 21:49:06 +0300

    using the Native Event Object

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
     increaseCounter(num) {
       this.counter += num;
     },
