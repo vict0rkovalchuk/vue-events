172e5786e3106342febdd5fe13f4fef6beac0fd7
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-12 22:36:06 +0300

    clearing the project to continue learning

--- app.js
+++ app.js
@@ -2,26 +2,19 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
-      confirmedName: ''
+      name: ''
     };
   },
   methods: {
-    confirmInput(event) {
-      this.confirmedName = this.name;
-      event.target.blur();
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     },
-    submitForm(event) {
-      // event.preventDefault();
+    add(num) {
+      this.counter = this.counter + num;
     },
-    setName(event) {
-      this.name = event.target.value;
-    },
-    increaseCounter(num) {
-      this.counter += num;
-    },
-    decreaseCounter(num) {
-      this.counter -= num;
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
     }
   }
 });
